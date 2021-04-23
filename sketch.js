8cb5a811aad4bd569ef1a57670ed4f5853d84f79
anita-thilakan <anitathilakan@gmail.com>
2021-04-23 17:38:56 +0530

    fresh commit

--- sketch.js
+++ sketch.js
@@ -1,69 +1,63 @@
 var path,mainCyclist;
 var pathImg,mainRacerImg1,mainRacerImg2;
-var cyclebcgell;
-var pinkcg,yellowcg,redcg
-var pinkopp,redopp;
-var pinkimg,redimg;
-var redoppfall,pinkoppfall
-var gameover,gameoverimg;
 
 var END =0;
 var PLAY =1;
 var gameState = PLAY;
 
 var distance=0;
-
-var pinkendstate,redendstate;
+var bell, pink, yellow,pink_fell, yellow_fell;
+var pink_group, yellow_group
+var cycle, gameoverimg,gameover;
+var obstacle_img,obstacle,obstacle_group
+var chance = 5
 
 function preload(){
-  pathImg = loadAnimation("Road.png");
-  mainRacerImg1 = loadAnimation("mainPlayer1.png","mainPlayer2.png");
-  mainRacerImg2= loadAnimation("mainPlayer3.png");
-  
-  cyclebell = loadSound("sound/bell.mp3")
-  
-  redimg =loadAnimation("/images/opponent7.png","/images/opponent8.png")
-  
-  pinkimg =loadAnimation("/images/opponent1.png","/images/opponent2.png")
-  
-  // pinkendstate = loadAnimation("/images/opponent1.png")
-  // redendstate = loadAnimation("/images/opponent8.png")
-  
-  redoppfall = loadImage('/images/opponent9.png')
-  pinkoppfall = loadImage('/images/opponent3.png')
-  
-  gameoverimg = loadAnimation("/images/gameOver.png")
-  
+  pathImg = loadImage("images/Road.png");
+  mainRacerImg1 = loadAnimation("images/mainPlayer1.png","images/mainPlayer2.png");
+  mainRacerImg2= loadAnimation("images/mainPlayer3.png");
+  pink = loadAnimation("opponent1.png","opponent2.png")
+  yellow = loadAnimation("opponent4.png", "opponent5.png")
+  bell = loadSound("sound/bell.mp3")
+  pink_fell = loadImage("opponent3.png")
+  yellow_fell = loadImage("opponent6.png")
+  gameoverimg  = loadImage("gameOver.png")
+  obstacle_img = loadImage("obstacle3.png")
   
 }
 
 function setup(){
   
-createCanvas(1500,300);
+createCanvas(500,300);
   
 // Moving background
 path=createSprite(100,150);
-path.addAnimation("path",pathImg);
+path.addImage(pathImg);
+path.velocityX = -(5+ Math.round(distance/100));
 
 //creating boy running
 mainCyclist  = createSprite(70,150,20,20);
 mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-mainCyclist.scale=0.05;
-mainCyclist.debug = "true"  
+mainCyclist.addAnimation("fell",mainRacerImg2)
+mainCyclist.scale=0.06;
+// mainCyclist.debug = true
+mainCyclist.setCollider("rectangle",0,0,90,90)
+
+  // groups
+  pink_group = new Group()
+  yellow_group = new Group()
+  obstacle_group = new Group()
   
-  
-  redcg = new Group()
-  pinkcg = new Group()
-  
-  gameover = createSprite(300,150)
-  gameover.addAnimation("over",gameoverimg)
-  gameover.scale = 0.5
+  // gameover
+  gameover  = createSprite(150,250,10,10)
+  gameover.addImage(gameoverimg)
+  gameover.scale = 0.5;
   gameover.visible = false
+  
 }
 
 function draw() {
   background(0);
-  // console.log(frameRate())
   
   drawSprites();
   textSize(20);
@@ -73,112 +67,122 @@ function draw() {
   if(gameState===PLAY){
   
    mainCyclist.y = World.mouseY;
-    
-    if(keyDown('space'))
-      {
-        cyclebell.play()
-      }
-    
-    var rand = Math.round(random(1,2))
-    if(rand === 1)
-       {
-    redcyclists()
-         
-       }
-    else 
-      {
-    pinkcyclists()
-        
-      }
   
    edges= createEdgeSprites();
    mainCyclist .collide(edges);
+    
+    // spawn cycles
+    cyclist()
+    if(frameCount % 75 == 0){
+      spawnObstacles()
+    }
   
-    
-    // distance calculation
-    distance = distance + Math.round(getFrameRate()/30)
-    // addchallenge
-   path.velocityX = -(5 + 2*distance/150);
-    
   //code to reset the background
   if(path.x < 0 ){
     path.x = width/2;
   }
     
-    if(redcg.isTouching(mainCyclist))
-      {
-        gameState = END
-        redopp.addImage("red",redoppfall)
-      }
+    if(keyDown("space")){
+      bell.play()
+    }
+    // console.log("test : ",frameRate());
     
-    if(pinkcg.isTouching(mainCyclist))
-      {
-        gameState = END
-        pinkopp.addImage("pink",pinkoppfall)
-      }
+    distance = distance + Math.round(getFrameRate()/30)
+    if(mainCyclist.isTouching(pink_group) ){
+      gameState = END
+      mainCyclist.changeAnimation("fell")
+      cycle.addAnimation("cycle",pink_fell)
+    }
     
+    if(mainCyclist.isTouching(yellow_group) ){
+      gameState = END
+      mainCyclist.changeAnimation("fell")
+      cycle.addAnimation("cycle",yellow_fell)
+    }
     
+    if(mainCyclist.isTouching(obstacle_group) ){
+      gameState = END
+      mainCyclist.changeAnimation("fell")
+       obstacle_group.setVelocityXEach(0)
+    }
  }
   
-  else if(gameState === END)
-    {
-      
-      gameover.visible = true
-      textSize(20)
-      text('press up arrow to restart',300,250)
-      path.velocityX = 0
-      mainCyclist.addAnimation("SahilRunning",mainRacerImg2)
-      
-      pinkcg.setLifetimeEach(-1)
-      redcg.setLifetimeEach(-1)
-      
-      pinkcg.setVelocityXEach(0)
-      redcg.setVelocityXEach(0)
-      
-      if(keyDown('up'))
-         {
-           reset();
-         }
-      
+  if(gameState == END){
+    
+    end()
+    
+  }
+  
+   if(chance >0){
+    if(mousePressedOver(gameover)){
+      console.log("chance: "+chance)
+
+        gameover.visible = false
+        distance = 0;
+        pink_group.destroyEach()
+        yellow_group.destroyEach()
+        mainCyclist.changeAnimation("SahilRunning")
+        gameState = PLAY
+        chance--;
+      }
+    
+  }
+  
+}
+
+function spawnObstacles(){
+  var choice;
+  obstacle = createSprite(300,60,10,10)
+  obstacle.addImage(obstacle_img)
+  obstacle.scale = 0.05;
+  
+  obstacle.velocityX = -(6 +Math.round(distance/100))
+  obstacle.lifeTime = 300/6
+  choice = Math.round(random(1,3))
+  console.log("val : "+choice)
+  if(choice == 1){
+    obstacle.y = Math.round(random(10,50))
+  }
+  else if( choice == 2 ){
+    obstacle.y = Math.round(random(250,300))
+  }
+  obstacle_group.add(obstacle)
+  
+}
+
+function cyclist(){
+  var choice;
+  if(frameCount % 150 == 0){
+    cycle = createSprite(450, Math.round(random(50,250)),10,10)
+    cycle.velocityX = -(4+ Math.round(distance/100))
+   
+    cycle.setLifetime = 150
+    cycle.scale = 0.06
+    choice = Math.round(random(1,2))
+    if(choice == 1){
+      cycle.addAnimation("cycle", pink)
+      pink_group.add(cycle)
     }
-}
-
-function reset()
-{
-  distance = 0
-  redcg.destroyEach()
-  pinkcg.destroyEach()
-  gameState = PLAY
-  gameover.visible = false
- mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-  
-}
-function redcyclists()
-{
-  if(frameCount % 200 ===0)
-     {
-         redopp = createSprite(300,random(25,300),10,10)
-         redopp.addAnimation("red",redimg)
-         redopp.scale = 0.05
-         redopp.velocityX = -(5 + distance/150)
-         redopp.lifetime= 300
-       redopp.debug = "true"
-       redcg.add(redopp)
-     }
+    else{
+      cycle.addAnimation("cycle",yellow)
+      yellow_group.add(cycle)
+    }
+    
+    // console.log(choice)
+    
+   
+    
+  }
   
 }
 
-
-function pinkcyclists()
-{
-  if(frameCount % 100 ===0)
-     {
-         pinkopp = createSprite(700,random(25,300),10,10)
-         pinkopp.addAnimation("pink",pinkimg)
-         pinkopp.scale = 0.05
-         pinkopp.velocityX = -(5 + distance/150)
-         pinkopp.lifetime=300
-       pinkcg.add(pinkopp)
-     }
+function end(){
+  gameover.visible = true
+    
+    path.velocityX = 0;    
+    pink_group.setVelocityXEach(0)
+    yellow_group.setVelocityXEach(0)
+  
+    // yellow_group.destroyEach()
   
 }
